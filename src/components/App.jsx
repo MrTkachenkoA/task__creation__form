3dafe8a003a70aac2544583356206e866d68fcd9
MikoreVikt <136119613+MrTkachenkoA@users.noreply.github.com>
2023-11-22 03:02:44 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,156 @@
-import { Routes, Route } from 'react-router-dom';
-import { lazy } from 'react';
-import { ToastContainer } from 'react-toastify';
-import { Layout } from './Layout/Layout';
-
-const Home = lazy(() => import('../pages/Home/Home'));
-const Movies = lazy(() => import('../pages/Movies'));
-const MoviesSearch = lazy(() => import('./MoviesSearch/MoviesSearch'));
-const MoviesDetails = lazy(() => import('../pages/MovieDetails/MovieDetails'));
-const MovieCast = lazy(() => import('../pages/MovieCast/MovieCast'));
-const MovieReviews = lazy(() => import('../pages/MovieReviews/MovieReviews'));
-const NotFound = lazy(() => import('../pages/NotFound/NotFound'));
+import React, { useState } from 'react';
+import Select from 'react-select';
 
 export const App = () => {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/movies" element={<Movies />}>
-            <Route index element={<MoviesSearch />} />
-            <Route path=":movieId" element={<MoviesDetails />}>
-              <Route path="cast" element={<MovieCast />} />
-              <Route path="reviews" element={<MovieReviews />} />
-            </Route>
-          </Route>
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+  const [selectedExecutor, setSelectedExecutor] = useState(
+    'Ще не вибрано жодної особи...'
+  );
+  const [taskText, setTaskText] = useState('');
+  const [dueDate, setDueDate] = useState('');
+  const [dueTime, setDueTime] = useState('');
+  const [file, setFile] = useState(null);
 
-      <ToastContainer />
-    </>
+  const executorByNameOptions = [
+    { label: 'Ще не вибрано жодної особи...' },
+    { label: 'Паламарчук Євген Анатолійович' },
+    { label: 'Севастьянов Володимир Миколайович' },
+    { label: 'Хома Олег Іванович' },
+  ];
+  const executorByGroupOptions = [
+    { value: 'Виберіть список' },
+    { value: 'Декани' },
+    { value: 'Завідуючі кафедрами' },
+    { value: 'Ректорат' },
+  ];
+
+  const handleExecutorChange = event => {
+    const selectedExecutor = event.target.value;
+    setSelectedExecutor(selectedExecutor);
+  };
+
+  const handleExecutorChangeByName = selectedOption => {
+    setSelectedExecutor(selectedOption.label);
+  };
+
+  const handleTaskTextChange = event => {
+    const text = event.target.value;
+    setTaskText(text);
+  };
+
+  const handleDueDateChange = event => {
+    const date = event.target.value;
+    setDueDate(date);
+  };
+
+  const handleDueTimeChange = event => {
+    const time = event.target.value;
+    setDueTime(time);
+  };
+
+  const handleFileChange = event => {
+    const selectedFile = event.target.files[0];
+    setFile(selectedFile);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setSelectedExecutor('Ще не вибрано жодної особи...');
+    setTaskText('');
+    setDueDate('');
+    setDueTime('');
+    setFile(null);
+    console.log(
+      'Form submitted!',
+      selectedExecutor,
+      taskText,
+      dueDate,
+      dueTime,
+      file
+    );
+  };
+
+  return (
+    <div>
+      <div className="header">
+        <h1 className="title">Створити нове доручення</h1>
+      </div>
+
+      <form className="form" onSubmit={handleSubmit}>
+        <label className="label">
+          <p className="p">Зі списків:</p>
+          <select
+            className="set"
+            onChange={handleExecutorChange}
+            value={selectedExecutor}
+          >
+            {executorByGroupOptions.map(option => (
+              <option key={option.value} value={option.value}>
+                {option.value}
+              </option>
+            ))}
+          </select>
+        </label>
+
+        <label className="label">
+          <p className="p">Знайти:</p>
+          <Select
+            className="exec"
+            options={executorByNameOptions}
+            value={selectedExecutor}
+            onChange={handleExecutorChangeByName}
+            isSearchable
+            placeholder="Введіть прізвище"
+          />
+        </label>
+
+        {selectedExecutor ? (
+          selectedExecutor.label ? (
+            <label className="label">
+              <p className="p">Виконавці: </p>
+              <p className="nobody">{selectedExecutor.label}</p>
+            </label>
+          ) : (
+            <label className="label">
+              <p className="p">Виконавці: </p>
+              <p className="nobody">{selectedExecutor}</p>
+            </label>
+          )
+        ) : (
+          <label className="label">
+            <p className="p">Виконавці: </p>
+            <p className="nobody">{selectedExecutor}</p>
+          </label>
+        )}
+
+        <label className="label">
+          <p className="p">Доручення:</p>
+          <textarea
+            className="text"
+            value={taskText}
+            onChange={handleTaskTextChange}
+            placeholder="Напишіть тут зміст"
+          />
+        </label>
+
+        <label className="label">
+          <p className="p">Виконати до:</p>
+          <input className="set" type="date" onChange={handleDueDateChange} />
+        </label>
+
+        <label className="label">
+          <p className="p">*Терміново до:</p>
+          <input className="set" type="time" onChange={handleDueTimeChange} />
+        </label>
+
+        <label className="label">
+          <p className="p">Додати файл:</p>
+          <input type="file" onChange={handleFileChange} />
+        </label>
+
+        <button className="btn" type="submit">
+          Створити доручення
+        </button>
+      </form>
+    </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="individual_project">
+    <BrowserRouter basename="task__creation__form">
       <App />
     </BrowserRouter>
   </React.StrictMode>
